276c58c9ce097481b5aea6fe11fa088dcc695785
Jens Eckert <capt.lupa@gmail.com>
2023-05-10 00:03:47 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() {
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const BenGPT());
+}
+
+class BenGPT extends StatefulWidget {
+  const BenGPT({super.key});
+
+  @override
+  State<BenGPT> createState() => _BenGPTState();
+}
+
+class _BenGPTState extends State<BenGPT> {
+  final random = Random();
+  String answer = "";
+  List<Text> history = [];
+  List<String> responses = [
+    "Check OPSWiki",
+    "It's on OPSWiki",
+    "*Stares at you for 8 seconds*",
+    "Did you check OPSWiki yet?",
+    "I put it on OPSWiki yesterday",
+  ];
+  TextEditingController controller = TextEditingController();
+
+  void processQuestion(value) {
+    controller.text = "";
+
+    setState(() {
+      answer = responses[random.nextInt(responses.length)];
+      history.insert(0, Text("You: " + value + "\nBen: " + answer));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextField question = TextField(
+      decoration: const InputDecoration(hintText: "Ask Ben a question:", border: OutlineInputBorder()),
+      onSubmitted: processQuestion,
+      controller: controller,
+    );
+
+    return MaterialApp(
+        title: "BenGPT",
+        home: Scaffold(
+            appBar: AppBar(title: const Text("BenGPT")),
+            body: Column(
+              children: [
+                Column(children: history),
+                Text(answer),
+                question,
+              ],
+            )));
+  }
+}
